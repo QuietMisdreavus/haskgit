fc1b93b244487d9bf35f7a648d80994c23925e99
QuietMisdreavus <grey@quietmisdreavus.net>
2020-04-22 11:44:27 -0600

    add tests for add-command that inspect output

--- src/Tests/Command/Add.hs
+++ src/Tests/Command/Add.hs
@@ -15,9 +15,14 @@ addCommandTests :: Test
 addCommandTests = TestList
     [ TestLabel "adds a regular file to the index" addSingleFile
     , TestLabel "adds an executable file to the index" addExecutableFile
+    , TestLabel "adds multiple files to the index" addMultipleFiles
     , TestLabel "incrementally adds files to the index" incrementallyAddFiles
     , TestLabel "adds a directory to the index" addDirectory
     , TestLabel "adds the repository root to the index" addRepoRoot
+    , TestLabel "is silent on success" silentOnSuccess
+    , TestLabel "fails for nonexistent files" nonexistentFailure
+    , TestLabel "fails for unreadable files" unreadableFailure
+    , TestLabel "fails if the index is locked" indexLockedFailure
     ]
 
 assertIndex :: [(Word32, String)] -> Assertion
@@ -39,6 +44,13 @@ addExecutableFile = TestCase $ runTest $ do
     testRunCommand ["add", "hello.txt"]
     assertIndex [(0o100755, "hello.txt")]
 
+addMultipleFiles :: Test
+addMultipleFiles = TestCase $ runTest $ do
+    writeTestFile "hello.txt" "hello"
+    writeTestFile "world.txt" "world"
+    testRunCommand ["add", "hello.txt", "world.txt"]
+    assertIndex [(0o100644, "hello.txt"), (0o100644, "world.txt")]
+
 incrementallyAddFiles :: Test
 incrementallyAddFiles = TestCase $ runTest $ do
     writeTestFile "hello.txt" "hello"
@@ -59,3 +71,40 @@ addRepoRoot = TestCase $ runTest $ do
     writeTestFile "a/b/c/file.txt" "content"
     testRunCommand ["add", "."]
     assertIndex [(0o100644, "a/b/c/file.txt")]
+
+silentOnSuccess :: Test
+silentOnSuccess = TestCase $ runTest $ do
+    writeTestFile "hello.txt" "hello"
+    out <- testRunCommand ["add", "hello.txt"]
+    assertCommandStatus 0 out
+    assertCommandStdout "" out
+    assertCommandStderr "" out
+
+nonexistentFailure :: Test
+nonexistentFailure = TestCase $ runTest $ do
+    out <- testRunCommand ["add", "no-such-file"]
+    assertCommandStderr
+        "fatal: pathspec 'no-such-file' did not match any files\n"
+        out
+    assertCommandStatus 128 out
+    assertIndex []
+
+unreadableFailure :: Test
+unreadableFailure = TestCase $ runTest $ do
+    writeTestFile "secret.txt" ""
+    makeTestFileUnreadable "secret.txt"
+    out <- testRunCommand ["add", "secret.txt"]
+    assertCommandStderr
+        "error: open('secret.txt'): Permission denied\n\
+            \fatal: adding files failed\n"
+        out
+    assertCommandStatus 128 out
+    assertIndex []
+
+indexLockedFailure :: Test
+indexLockedFailure = TestCase $ runTest $ do
+    writeTestFile "file.txt" ""
+    writeTestFile ".git/index.lock" ""
+    out <- testRunCommand ["add", "file.txt"]
+    assertCommandStatus 128 out
+    assertIndex []
--- src/Tests/CommandHelper.hs
+++ src/Tests/CommandHelper.hs
@@ -3,18 +3,24 @@ module Tests.CommandHelper
     , mkTmpPath
     , writeTestFile
     , makeTestFileExecutable
+    , makeTestFileUnreadable
+    , CommandOutput(..)
     , testRunCommand
     , runTest
+    , assertCommandStatus
+    , assertCommandStdout
+    , assertCommandStderr
     ) where
 
 import Data.Bitraversable
 import Control.Exception
 import qualified Data.Map.Strict as Map
 import System.Directory
-import System.Exit (ExitCode)
+import System.Exit (ExitCode(..))
 import System.FilePath
 import System.IO
 import System.Posix.IO
+import Test.HUnit
 
 import Command
 import Command.Base
@@ -44,11 +50,22 @@ makeTestFileExecutable name = do
         $ setOwnerWritable True
         $ emptyPermissions
 
-testRunCommand :: [String] -> IO ExitCode
+makeTestFileUnreadable :: String -> IO ()
+makeTestFileUnreadable name = do
+    fullPath <- (</> name) <$> mkTmpPath
+    setPermissions fullPath emptyPermissions
+
+data CommandOutput = CommandOutput
+    { outputCode :: ExitCode
+    , outputStdout :: Handle
+    , outputStderr :: Handle
+    }
+
+testRunCommand :: [String] -> IO CommandOutput
 testRunCommand argv = do
     (fakeStdin, _) <- mkPipe
-    (_, fakeStdout) <- mkPipe
-    (fakeStderr, _) <- mkPipe
+    (outputStdout, fakeStdout) <- mkPipe
+    (outputStderr, fakeStderr) <- mkPipe
     repoDir <- mkTmpPath
     let env = CommandBase
             { commDir = repoDir
@@ -58,10 +75,30 @@ testRunCommand argv = do
             , commStdout = fakeStdout
             , commStderr = fakeStderr
             }
-    runCommand env
+    code <- runCommand env
+    hClose fakeStdin
+    hClose fakeStdout
+    hClose fakeStderr
+    return $ CommandOutput code outputStdout outputStderr
 
 runTest :: IO a -> IO a
 runTest action = do
     repoPath <- mkTmpPath
     testRunCommand ["init", repoPath]
     finally action $ removePathForcibly repoPath
+
+assertCommandStatus :: Int -> CommandOutput -> Assertion
+assertCommandStatus expected (CommandOutput { outputCode = code }) =
+    if expected == 0
+        then assertEqual "" ExitSuccess code
+        else assertEqual "" (ExitFailure expected) code
+
+assertCommandStdout :: String -> CommandOutput -> Assertion
+assertCommandStdout expected (CommandOutput { outputStdout = h }) = do
+    actual <- hGetContents h
+    assertEqual "" expected actual
+
+assertCommandStderr :: String -> CommandOutput -> Assertion
+assertCommandStderr expected (CommandOutput { outputStderr = h }) = do
+    actual <- hGetContents h
+    assertEqual "" expected actual
